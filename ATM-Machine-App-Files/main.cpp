87f2dece39fa7f0f2dea53c04ccd98c95473232b
Muhammad <maej_01@hotmail.com>
2019-04-13 15:53:26 +0000

    first push

---
+++ ATM-Machine-App-Files/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdlib>
+
+
+using namespace std;
+
+
+
+
+
+int main()
+{
+    int choice;
+
+
+   cout <<"\t\tWELCOME TO MAE BANK\n";
+   cout <<"\t\t*******************\n";
+
+
+   cout <<"Welcome our valid customer what would you like to do today\n";
+   cout <<"\t1) WITHDRAWAL\n\t2) DEPOSITE\n\t3) CHECK BALANCE\n";
+   cin >>choice;
+
+
+
+
+
+
+
+    return 0;
+}
